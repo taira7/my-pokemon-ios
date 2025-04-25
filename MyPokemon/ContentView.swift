0298c454afed8efe3e7bf303957241ec256577a9
taira7 <0ge3855.0bj121j@gmail.com>
2025-04-25 15:42:37 +0900

    Initial Commit

---
+++ MyPokemon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyPokemon
+//
+//  Created by 平良将泰 on 2025/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyPokemonTests/MyPokemonTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyPokemonTests.swift
+//  MyPokemonTests
+//
+//  Created by 平良将泰 on 2025/04/25.
+//
+
+import Testing
+@testable import MyPokemon
+
+struct MyPokemonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
