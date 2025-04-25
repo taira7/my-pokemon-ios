477f807013587a9edd9e578ae3c1a98636c96ade
taira7 <0ge3855.0bj121j@gmail.com>
2025-04-25 15:42:37 +0900

    Initial Commit

---
+++ MyPokemon/MyPokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPokemonApp.swift
+//  MyPokemon
+//
+//  Created by 平良将泰 on 2025/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MyPokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
